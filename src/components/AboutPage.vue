60919ee78007008910aee973f1e656fa55c371a1
laylark <laylan@gmail.com>
2023-02-04 15:10:13 -0600

    Add AboutPage component

---
+++ src/components/AboutPage.vue
@@ -0,0 +1,39 @@
+<script setup></script>
+
+<template>
+  <div name="about" class="bg-white-bg text-gray-800 flex items-center">
+    <div class="flex flex-col justify-center items-center h-full p-16">
+      <div class="max-w-[1000px] w-full grid grid-cols-2 gap-8">
+        <div class="sm:text-right pb-8">
+          <p class="text-4xl font-bold inline border-b-4 border-orange">
+            About
+          </p>
+        </div>
+        <div></div>
+      </div>
+      <div class="max-w-[1000px] grid sm:grid-cols-2 gap-8">
+        <div class="sm:text-right text-4xl font-bold">
+          <p>Hi, I'm Layla. I build cool things for the web and beyond.</p>
+        </div>
+        <div>
+          <p>
+            My favorite part of being a full-stack developer is the rapid pace
+            of technological change<span>&#8212;</span>
+            meaning there are always new learning opportunities. My goal is to
+            create meaningful software to help others increase productivity at
+            work, home, or anywhere. I have the most fun building data-driven
+            projects using databases, Python, Javascript, and APIs; however, I
+            do enjoy making responsive front-end web applications.
+          </p>
+          <p class="py-4">
+            I am also a mom, registered dietitian nutritionist, adjunct
+            instructor, and avid community volunteer. You can find me at many
+            local tech meetups in OKC!
+          </p>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<style></style>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,11 +1,13 @@
 <script setup>
 import HomePage from "../components/HomePage.vue";
 import ProjectsPage from "../components/ProjectsPage.vue";
+import AboutPage from "../components/AboutPage.vue";
 </script>
 
 <template>
   <main>
     <HomePage />
+    <AboutPage />
     <ProjectsPage />
   </main>
 </template>
